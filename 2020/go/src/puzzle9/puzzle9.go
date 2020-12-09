801ef8060ad9fc4e19173b02fca27912c1df11ef
Dariusz Parol <parol.mail@gmail.com>
2020-12-09 08:54:49 +0100

    2020 - puzzle 9

---
+++ 2020/go/src/puzzle9/puzzle9.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"readers"
+	"strings"
+	"utils"
+)
+
+const pLen int = 25
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input = bytes.TrimSpace(input)
+
+	ints, err := readers.ReadInts(strings.NewReader(string(input)))
+	if err != nil {
+		panic(err)
+	}
+
+	part1, part2 := 0, 0
+
+	for s := pLen; s < len(ints); s++ {
+		fnd := false
+		preamble := ints[s-pLen : s]
+		for i := 0; i < pLen; i++ {
+			for j := 0; j < pLen; j++ {
+				if ints[s] == (preamble[i] + preamble[j]) {
+					fnd = true
+					break
+				}
+			}
+			if fnd {
+				break
+			}
+		}
+		if !fnd {
+			part1 = ints[s]
+			break
+		}
+	}
+
+	total := 0
+	firstIdx, lastIdx := 0, 0
+	for i, num1 := range ints {
+		total = num1
+		for j := i + 1; j < len(ints); j++ {
+			total += ints[j]
+			if total == part1 {
+				firstIdx = i
+				lastIdx = j
+				break
+			}
+		}
+		if total == part1 {
+			break
+		}
+	}
+
+	minV := utils.Min(ints[firstIdx : lastIdx+1])
+	maxV := utils.Max(ints[firstIdx : lastIdx+1])
+	part2 = minV + maxV
+
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
+}
