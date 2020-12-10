e0f7c3211ab451ecde39d5ec2d205c632dd5cc92
Dariusz Parol <parol.mail@gmail.com>
2020-12-10 18:52:21 +0100

    2020 - puzzle 10

---
+++ 2020/go/src/puzzle10/puzzle10.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"readers"
+	"sort"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input = bytes.TrimSpace(input)
+
+	ints, err := readers.ReadInts(strings.NewReader(string(input)))
+	if err != nil {
+		panic(err)
+	}
+
+	ints = append(ints, 0)
+	sort.Ints(ints)
+	ints = append(ints, ints[len(ints)-1]+3)
+
+	part1, part2 := 0, 0
+	d1, d3 := 0, 0
+	ways := []int{1}
+
+	for i := 1; i < len(ints); i++ {
+		d := ints[i] - ints[i-1]
+		if d == 1 {
+			d1++
+		} else if d == 3 {
+			d3++
+		}
+
+		ways = append(ways, 0)
+		for j := 0; j < i; j++ {
+			if ints[i]-ints[j] <= 3 {
+				ways[i] += ways[j]
+			}
+		}
+	}
+
+	part1 = d1 * d3
+	part2 = ways[len(ways)-1]
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
+}
