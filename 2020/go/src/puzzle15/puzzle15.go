4a0d3e912e85d79bddb0d26a0db775f2f59d3d20
Dariusz Parol <parol.mail@gmail.com>
2020-12-15 07:02:00 +0100

    2020 - puzzle 15

---
+++ 2020/go/src/puzzle15/puzzle15.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const input string = "19,0,5,1,10,13"
+const part1Limit int = 2020
+const part2Limit int = 30000000
+
+func main() {
+	part1, part2 := 0, 0
+	indices := make(map[int]int)
+
+	nums := strings.Split(input, ",")
+	for i, nStr := range nums {
+		n, _ := strconv.Atoi(nStr)
+		indices[n] = i
+	}
+
+	idx := len(nums) - 1
+	n := 0
+	idx++
+	for idx < part2Limit-1 {
+		if _, ok := indices[n]; ok {
+			prevIdx := indices[n]
+			indices[n] = idx
+			n = idx - prevIdx
+		} else {
+			indices[n] = idx
+			n = 0
+		}
+
+		if idx < part1Limit-1 {
+			part1 = n
+		}
+		part2 = n
+		idx++
+	}
+
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
+}
