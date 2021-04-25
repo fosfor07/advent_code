0430e7b7d0a2648e715f12ca6de5a80097e12271
Dariusz Parol <parol.mail@gmail.com>
2021-04-25 23:42:37 +0200

    Added solution of part 1 to puzzle 22.

--- 2020/go/src/puzzle22/puzzle22.go
+++ 2020/go/src/puzzle22/puzzle22.go
@@ -31,39 +31,31 @@ func subGame(deck1, deck2 []int, card1, card2 int) int {
 	var hist1, hist2 [][]int
 
 	for len(sDeck1) > 0 && len(sDeck2) > 0 {
-		sameCards := checkHistory(hist1, sDeck1)
-		if sameCards {
+		if checkHistory(hist1, sDeck1) {
 			return winner
 		}
-		sameCards = checkHistory(hist2, sDeck2)
-		if sameCards {
+		if checkHistory(hist2, sDeck2) {
 			return winner
 		}
 
 		hist1 = append(hist1, sDeck1)
 		hist2 = append(hist2, sDeck2)
 
-		sCard1 := sDeck1[0]
-		sDeck1 = sDeck1[1:]
-		sCard2 := sDeck2[0]
-		sDeck2 = sDeck2[1:]
+		sCard1, sCard2 := sDeck1[0], sDeck2[0]
+		sDeck1, sDeck2 = sDeck1[1:], sDeck2[1:]
 
 		if len(sDeck1) >= sCard1 && len(sDeck2) >= sCard2 {
 			sWinner := subGame(sDeck1, sDeck2, sCard1, sCard2)
 			if sWinner == 1 {
-				sDeck1 = append(sDeck1, sCard1)
-				sDeck1 = append(sDeck1, sCard2)
+				sDeck1 = append(sDeck1, sCard1, sCard2)
 			} else {
-				sDeck2 = append(sDeck2, sCard2)
-				sDeck2 = append(sDeck2, sCard1)
+				sDeck2 = append(sDeck2, sCard2, sCard1)
 			}
 		} else {
 			if sCard1 > sCard2 {
-				sDeck1 = append(sDeck1, sCard1)
-				sDeck1 = append(sDeck1, sCard2)
+				sDeck1 = append(sDeck1, sCard1, sCard2)
 			} else {
-				sDeck2 = append(sDeck2, sCard2)
-				sDeck2 = append(sDeck2, sCard1)
+				sDeck2 = append(sDeck2, sCard2, sCard1)
 			}
 		}
 	}
@@ -109,16 +101,43 @@ func main() {
 		}
 	}
 
+	// part 1
+	deck1p1 := make([]int, len(deck1))
+	deck2p1 := make([]int, len(deck2))
+	copy(deck1p1, deck1)
+	copy(deck2p1, deck2)
+
+	for len(deck1p1) > 0 && len(deck2p1) > 0 {
+		card1, card2 := deck1p1[0], deck2p1[0]
+		deck1p1, deck2p1 = deck1p1[1:], deck2p1[1:]
+
+		if card1 > card2 {
+			deck1p1 = append(deck1p1, card1, card2)
+		} else {
+			deck2p1 = append(deck2p1, card2, card1)
+		}
+	}
+
+	winDeck := deck2p1
+	if len(deck1p1) > 0 {
+		winDeck = deck1p1
+	}
+
+	value := 1
+	for i := len(winDeck) - 1; i >= 0; i-- {
+		part1 += value * winDeck[i]
+		value++
+	}
+
+	// part 2
 	var hist1, hist2 [][]int
 
 	for len(deck1) > 0 && len(deck2) > 0 {
-		sameCards := checkHistory(hist1, deck1)
-		if sameCards {
+		if checkHistory(hist1, deck1) {
 			deck2 = nil
 			break
 		}
-		sameCards = checkHistory(hist2, deck2)
-		if sameCards {
+		if checkHistory(hist2, deck2) {
 			deck2 = nil
 			break
 		}
@@ -126,39 +145,31 @@ func main() {
 		hist1 = append(hist1, deck1)
 		hist2 = append(hist2, deck2)
 
-		card1 := deck1[0]
-		deck1 = deck1[1:]
-		card2 := deck2[0]
-		deck2 = deck2[1:]
+		card1, card2 := deck1[0], deck2[0]
+		deck1, deck2 = deck1[1:], deck2[1:]
 
 		if len(deck1) >= card1 && len(deck2) >= card2 {
 			winner := subGame(deck1, deck2, card1, card2)
 			if winner == 1 {
-				deck1 = append(deck1, card1)
-				deck1 = append(deck1, card2)
+				deck1 = append(deck1, card1, card2)
 			} else {
-				deck2 = append(deck2, card2)
-				deck2 = append(deck2, card1)
+				deck2 = append(deck2, card2, card1)
 			}
 		} else {
 			if card1 > card2 {
-				deck1 = append(deck1, card1)
-				deck1 = append(deck1, card2)
+				deck1 = append(deck1, card1, card2)
 			} else {
-				deck2 = append(deck2, card2)
-				deck2 = append(deck2, card1)
+				deck2 = append(deck2, card2, card1)
 			}
 		}
 	}
 
-	var winDeck []int
+	winDeck = deck2
 	if len(deck1) > 0 {
 		winDeck = deck1
-	} else {
-		winDeck = deck2
 	}
 
-	value := 1
+	value = 1
 	for i := len(winDeck) - 1; i >= 0; i-- {
 		part2 += value * winDeck[i]
 		value++
