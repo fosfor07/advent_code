e61020e8dded81f37a3643d6d1cc3249718d62b5
Dariusz Parol <parol.mail@gmail.com>
2021-06-27 21:54:50 +0200

    2015 - puzzle 2

---
+++ 2015/go/src/puzzle2/puzzle2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"readers"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input = bytes.TrimSpace(input)
+
+	lines, err := readers.ReadLines(strings.NewReader(string(input)))
+	if err != nil {
+		panic(err)
+	}
+
+	part1, part2 := 0, 0
+
+	for _, line := range lines {
+		dimensions := strings.Split(line, "x")
+		l, _ := strconv.Atoi(dimensions[0])
+		w, _ := strconv.Atoi(dimensions[1])
+		h, _ := strconv.Atoi(dimensions[2])
+
+		d1, d2 := 0, 0
+
+		if l < w {
+			d1, d2 = l, w
+		} else {
+			d1, d2 = w, l
+		}
+
+		if h < d2 {
+			d2 = h
+		}
+
+		part1 += 2*l*w + 2*w*h + 2*h*l + d1*d2
+		part2 += 2*d1 + 2*d2 + l*w*h
+	}
+
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
+}
